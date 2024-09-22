6b1d7c8450c4fb7f89272319958da948de8c64ea
rp4563 <rp4563@srmist.edu.in>
2024-09-22 17:56:53 +0530

    build successfull

--- src/components/apiForm.jsx
+++ src/components/apiForm.jsx
@@ -132,7 +132,7 @@ const ApiForm = () => {
         const formattedData = formatData(data, dataFormat)
 
         try {
-            const res = await fetch('http://localhost:4000/bfhl', {
+            const res = await fetch('https://bajaj.rp4563.workers.dev', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
